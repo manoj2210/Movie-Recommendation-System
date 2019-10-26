04fd40857e3bb02b7de2bc28a671926a8ee39531
manoj2210 <45814826+manoj2210@users.noreply.github.com>
2019-10-26 23:58:39 +0530

    Ui organised and added

--- ui.r
+++ ui.r
@@ -1,54 +1,79 @@
 #ui.R
 library(shiny)
-library(shinythemes)
-
+library(shinydashboard)
+library(excelR)
 
 #static array for genre list
 genre_list <- c("Select","Action", "Adventure", "Animation", "Children", 
                 "Comedy", "Crime","Documentary", "Drama", "Fantasy",
                 "Film.Noir", "Horror", "Musical", "Mystery","Romance",
                 "Sci.Fi", "Thriller", "War", "Western")
+search <- read.csv("movies.csv", stringsAsFactors=FALSE)
+m<-search$title
+n<-search$genres
+data = data.frame(
+  Title=m,
+  Genre=n
+)
+columns = data.frame(title=c('Title', 'Genre'),
+                     width= c(600, 500),
+                     type=c('text', 'text')) 
+rows=data.frame(title=1,
+                width=30
+)
 
-#shiny code to display web page
-shinyUI(fluidPage(
-  wellPanel("Movie Recommendation Search Engine"),
-  #shinythemes::themeSelector(),
-  #tags$style("body {background: url(https://i.ytimg.com/vi/ifVEMkQ9BaY/maxresdefault.jpg) no-repeat center center fixed; 
-  # background-size: cover;   filter:grayscale(100%);}"),
-  
-  tags$style("body {background: url(http://www.wallpaperup.com/wallpaper/download/858715) no-repeat center center fixed; 
-             background-size: cover;   filter:grayscale(100%);}"),
-  
-  
-  fluidRow(
-    
-    column(4, wellPanel(h3("Select Movie Genres You Prefer (order matters):")),
-           wellPanel(
-             selectInput("input_genre", "Genre #1",
-                         genre_list),
-             selectInput("input_genre2", "Genre #2",
-                         genre_list),
-             selectInput("input_genre3", "Genre #3",
-                         genre_list)
-             #submitButton("Update List of Movies")
-           )),
-    
-    column(4,  wellPanel(h3("Select Movies You Like of these Genres:")),
-           wellPanel(
-             # This outputs the dynamic UI component
-             uiOutput("ui"),
-             uiOutput("ui2"),
-             uiOutput("ui3")
-             #submitButton("Get Recommendations")
-           )),
-    
-    column(4,
-           wellPanel(h3("You Might Like The Following Movies Too!")),
-           wellPanel(
-             tableOutput("table")
-             #verbatimTextOutput("dynamic_value")
-           ))
+shinyUI(dashboardPage(
+  dashboardHeader(title = "Movie Recommendation System"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Recommend Movie", tabName = "RecommendMovie", icon = icon("dashboard")),
+      menuItem("List Movies", tabName = "ListMovies", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+
+      tabItem(tabName = "RecommendMovie",
+              fluidRow(
+                
+                column(4, wellPanel(h3("Select Movie Genres You Prefer (order matters):")),
+                       wellPanel(
+                         selectInput("input_genre", "Genre #1",
+                                     genre_list),
+                         selectInput("input_genre2", "Genre #2",
+                                     genre_list),
+                         selectInput("input_genre3", "Genre #3",
+                                     genre_list)
+                         #submitButton("Update List of Movies")
+                       )),
+                
+                column(4,  wellPanel(h3("Select Movies You Like of these Genres:")),
+                       wellPanel(
+                         # This outputs the dynamic UI component
+                         uiOutput("ui"),
+                         uiOutput("ui2"),
+                         uiOutput("ui3")
+                         #submitButton("Get Recommendations")
+                       )),
+                
+                column(4,
+                       wellPanel(h3("You Might Like The Following Movies Too!")),
+                       wellPanel(
+                         tableOutput("table")
+                         #verbatimTextOutput("dynamic_value")
+                       ))
+              )
+              
+      ),
+      
+      tabItem(tabName = "ListMovies",
+              
+             fluidRow( excelTable(data=data,columns = columns,pagination = 50,minDimensions = c(2,20),search = TRUE,rowHeight = rows
+                                  )
+                       
+             )
+      )
+    )
   )
-  
-  
-))
+)
+)
